0ec8c8c10389ed9f9ae0c86878a3185a1c6aea61
fanxiaolong <developer@lunarhook.com>
2021-05-10 05:21:40 +0800

    1

--- lunarhook/upgrade.sh
+++ lunarhook/upgrade.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 brew upgrade
-brew install node
-brew install watchman
-brew install git
-brew install go
-brew install yarn
-brew install flow
+brew install adoptopenjdk8
+brew install apr
+brew install apr-util
+brew install clang-format
 brew install cocoapods
 brew install coreutils
-brew install maven
-brew install clang-format
+brew tap dart-lang/dart
 brew install dart
-brew cask install android-sdk
-brew cask install homebrew/cask-versions/adoptopenjdk8
+brew install gettext
+brew install git
+brew install go
+brew install httpd
+brew install icu4c
+brew install kubernetes-cli
+brew install maven
+brew install node
+brew install nghttp2
+brew install nginx
+brew install openjdk
+brew install pcre
+brew install pcre2
+brew install readline
+brew install sqlite
+brew install watchman
+brew install yarn
+brew install ideviceinstaller ios-webkit-debug-proxy
+brew install --cask android-sdk
+brew install --cask homebrew/cask-versions/adoptopenjdk8
 brew cleanup
 npm config set registry https://registry.npm.taobao.org --global
 npm config set disturl https://npm.taobao.org/dist --global
 yarn global add react-native-cli
 npm install -g npm-check-updates
-git stash
 git pull
-git stash pop
 ncu -u -x react-native
 yarn global add @tarojs/cli@latest
 yarn config set registry https://registry.npm.taobao.org --global
